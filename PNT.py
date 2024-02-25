e0ad6cb8c35b5b5b5a04dc6d1c62e98e0f5e9a3b
pratyasha-dakshinray <pratyashaprakash@gmail.com>
2024-02-25 20:41:09 +0530

    fixed pylance error

--- PNT.py
+++ PNT.py
@@ -1,4 +1,8 @@
 import phonenumbers
 from phonenumbers import geocoder
+from test import number
 import folium
 
+check_number = phonenumbers.parse(number)
+number_location = geocoder.description_for_number(check_number, "en")
+print(number_location)
