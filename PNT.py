74b0802c14fed4582c374c9c5abd18926a4b7efa
pratyasha-dakshinray <pratyashaprakash@gmail.com>
2024-02-26 10:16:03 +0530

    Got the location

--- PNT.py
+++ PNT.py
@@ -3,6 +3,12 @@ from phonenumbers import geocoder
 from test import number
 import folium
 
-check_number = phonenumbers.parse(number)
+check_number = phonenumbers.parse("+919668059276")
 number_location = geocoder.description_for_number(check_number, "en")
 print(number_location)
+
+
+from phonenumbers import carrier
+service_provider = phonenumbers.parse(number)
+print(carrier.name_for_number(service_provider, "en"))
+
